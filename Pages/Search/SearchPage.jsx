c6882c61441cf6791e36c15abb82b39466ea60d2
Kozicki Matthieu <Matthieu@desktop-vnc8ku2.home>
2020-09-30 10:34:13 +0200

    [ADD] style for head's page

--- Pages/Search/SearchPage.jsx
+++ Pages/Search/SearchPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { Container } from "@material-ui/core";
 
 export default function SearchPage() {
   return (
-    <View>
-      <Text>Card displayer</Text>
-    </View>
+    <Container style={{backgroundColor: "grey", height: "10%", textAlign: 'center'}}>
+      <Text><br/>bar de recherche<br/></Text>
+      <button>Most viral</button>
+      <button>User submitted</button>
+      <button>Hightest Scoring</button>
+    </Container>
   );
 }
