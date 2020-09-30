ae0e572178de6dab63b88dacbeb0310cbb5cf5c9
Yoan Saint Juste <yoan.saint-juste@epitech.eu>
2020-09-30 17:43:58 +0200

    [REMOVED] Removed Material Ui package

--- Pages/Search/SearchPage.jsx
+++ Pages/Search/SearchPage.jsx
@@ -1,26 +1,10 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
-import InputAdornment from '@material-ui/core/InputAdornment';
-import TextField from '@material-ui/core/TextField';
-import Paper from '@material-ui/core/Paper';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import Grid from '@material-ui/core/Grid';
-import MenuItem from '@material-ui/core/MenuItem';
-import Checkbox from '@material-ui/core/Checkbox';
-import Select from '@material-ui/core/Select';
-import Box from '@material-ui/core/Box';
 
 export default function SearchPage() {
   return (
     <View>
       <Text>Card displayer</Text>
-
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Text>xs=12</Text>
-        </Grid>
-      </Grid>
-
     </View>
   );
 }
