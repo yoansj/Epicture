d0a86540641f9f7bbf37603cb93c9e6414264957
Kozicki Matthieu <Matthieu@desktop-vnc8ku2.home>
2020-09-30 19:27:05 +0200

    [ADD] import for picker

--- Pages/Search/SearchPage.jsx
+++ Pages/Search/SearchPage.jsx
@@ -1,25 +1,61 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+import {Picker} from '@react-native-community/picker';
+import { TextInput } from 'react-native';
 
 export default function SearchPage() {
   return (
     <View>
-      <Text>Card displayer</Text>
+      <View style={styles.myTop}>
+
+
+      <TextInput style={styles.myBar}/>
+
+          <Picker selectedValue={"Most Scoring"}
+          style={styles.scrolLeft} >
+            <Picker.Item label="Most Scoring" value="java" />
+            <Picker.Item label="JavaScript" value="js" />
+          </Picker>
+
+          <Picker selectedValue={"Random"}
+          style={styles.scrolLeft} >
+            <Picker.Item label="Random" value="java" />
+            <Picker.Item label="JavaScript" value="js" />
+          </Picker>
+      </View>
+
+
+      <Text>print les images</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   myTop: {
-    backgroundColor: "grey",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    alignContent: "flex-start",
+    backgroundColor: "#566573",
     width: "100%",
+    height: "45%"
   },
-  image: {
-    width: 300,
-    height: 300,
+  scrolLeft: {
+    textAlign: "center",
+    height: 20,
+    width: 175,
   },
-  tinyLogo: {
-    width: 50,
-    height: 50,
+  myBar: {
+    marginLeft: 80,
+    marginTop: 60,
+    width: 200,
+    height: 40,
+    borderColor: 'black',
+    backgroundColor: "#808B96",
+    borderTopLeftRadius: 7,
+    borderTopRightRadius: 7,
+    borderBottomRightRadius: 7,
+    borderBottomLeftRadius: 7,
+    borderWidth: 1 , alignSelf: "center"
   },
 });
