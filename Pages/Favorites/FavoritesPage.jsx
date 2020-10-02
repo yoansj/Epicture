f4f271de997ebbe0fa2672cccd1fabdeb7c65345
Kozicki Matthieu <Matthieu@desktop-vnc8ku2.home>
2020-10-02 15:07:01 +0200

    [TEST][ADD] favorites pages style

--- Pages/Favorites/FavoritesPage.jsx
+++ Pages/Favorites/FavoritesPage.jsx
@@ -1,10 +1,36 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text ,StyleSheet} from "react-native";
+import { Container, Card, Icon, Header, Left, Right, Button} from 'native-base';
+
 
 export default function FavoritesPage() {
   return (
-    <View>
-      <Text>Card displayer</Text>
-    </View>
+    <Container>
+      <Header rounded>
+        <Text style={{marginTop: 17}}>Favorites </Text>
+      </Header>
+      <Card style={styles.myWarning}>
+        <Text>Log In please </Text>
+      </Card>
+      <Container style={styles.myMiddle}>
+        <Icon name="flask" />
+        <Text>Wow !</Text>
+        <Text>There is some space here</Text>
+      </Container>
+    </Container>
   );
 }
+const styles = StyleSheet.create({
+  myMiddle: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white"
+  },
+  myWarning: {
+    backgroundColor: 'red',
+    alignItems: 'center',
+    height: 60,
+    justifyContent: "center"
+  }
+});
